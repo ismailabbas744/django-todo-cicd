d7079a8b047ebcc4c0b18acaa5fcaf448d56634c
ismailabbas744 <129492585+ismailabbas744@users.noreply.github.com>
2026-05-15 14:39:36 +0500

    Update tests.py

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class SimpleUnitTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create_user(username='testuser', password='password123')
+        self.assertEqual(user.username, 'testuser')
